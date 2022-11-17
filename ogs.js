b4c8452d2249d14e10944a8d043f4315658b9e66
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2022-11-17 17:31:01 +0900

    update ogs파일 에러핸들링

--- ogs.js
+++ ogs.js
@@ -2,10 +2,14 @@ const ogs = require("open-graph-scraper");
 
 const getOg = async (url) => {
   let res;
-  await ogs({ url }).then((data) => {
-    const { error, result, response } = data;
-    res = result;
-  });
+  await ogs({ url })
+    .then((data) => {
+      const { error, result, response } = data;
+      res = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   return res;
 };
 
